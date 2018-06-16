6f1b2ba0999ed54777848f41d58be9905775debf
nanoTech Corporation <thiagofigcosta@hotmail.com>
2018-06-16 19:34:48 -0300

    modified egg holder

--- Genetic/egg_holder.py
+++ Genetic/egg_holder.py
@@ -5,7 +5,7 @@ import genetic as gen
 
 
 eggholder=lambda x:-(x[1]+47)*np.sin(np.sqrt(abs(x[1]+(x[0]/2)+47)))-x[0]*np.sin(np.sqrt(abs(x[0]-(x[1]+47))))
-eggholderminfinder=gen.Genetic(eggholder,2,300,-512,512,True,0.1,0.7,True,-1500,True)
+eggholderminfinder=gen.Genetic(eggholder,2,300,-512,512,True,0.2,0.6,True,-1500,True)
 eggholderminfinder.run()
 eggholderminfinder.showNotables()
 #eggholderminfinder.showPopulation()
